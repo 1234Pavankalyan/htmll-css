400dbe5c91c6fec7d0be4468d68766cb5ce4e3f2
1234Pavankalyan <115170652+1234Pavankalyan@users.noreply.github.com>
2023-04-11 10:33:11 +0530

    Add files via upload

---
+++ td.java
@@ -0,0 +1,36 @@
+ <td rowspan="3">k</td>
+             <tr>
+                <td rowspan="2">c</td>
+                  <td>y</td>
+                   <td>a+</td>
+                    <td>a</td>
+                  <tr>
+                   <td>z</td>
+                   <td>a</td>
+                    <td>a</td>
+                  </tr>
+                 
+                 <tr>
+                  <td rowspan="2"> B</td>
+                  <td>y</td>
+                   <td>a+</td>
+                  <td>a</td>
+                  <tr>
+                  <td>z</td>
+                  <td>a</td>
+                 <td>a</td>
+                </tr>
+                <tr>
+                    <td rowspan="2"> Z</td>
+                    <td>y</td>
+                    <td>a+</td>
+                     <td>a</td>
+                     <tr>
+                     <td>z</td>
+                     <td>a</td>
+                    <td>a</td>
+                   </tr>
+            </tr> 
+         </tr>
+        
+       
